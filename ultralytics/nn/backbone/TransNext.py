d8a185420c2b383e7ca4ba006f1fa1f6a49f26dc
Shengchen Li <shengcli@126.com>
2024-03-28 17:11:24 +0800

    第一次提交文件

---
+++ ultralytics/nn/backbone/TransNext.py
@@ -0,0 +1,6 @@
+try:
+    import swattention
+    from ultralytics.nn.backbone.TransNeXt.TransNext_cuda import *
+except ImportError as e:
+    from ultralytics.nn.backbone.TransNeXt.TransNext_native import *
+    pass
